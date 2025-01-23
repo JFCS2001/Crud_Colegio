eb6eee2e6c918aa45782e8c8399d715e96ee63b4
JFCS2001 <132624100+JFCS2001@users.noreply.github.com>
2025-01-23 16:40:50 -0500

    Creación de usuarios(Vista y verificación de conexion a la BD)

---
+++ Models/Documentos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Crud_Colegio.Models
+{
+    public class Documentos
+    {
+        [Key]
+        [Column("ID_DOC")]
+        public int Id_Doc { get; set; }
+
+        [Column("NOMBRE_DOC")]
+        public string Nombre_Doc { get; set; }
+    }
+}
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,40 +1,51 @@
-﻿using static Crud_Colegio.Models.Entities;
+﻿using Crud_Colegio.Models;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace Crud_Colegio.Models
+public class Usuario
 {
-    public class Usuario
-    {
-            [Key]
-            [Column("ID_USER")]
-            public int Id_User { get; set; }
+    [Key]
+    [Column("ID_USER")]
+    public int Id_User { get; set; }
 
-            [Column("ID_DOC")]
-            public int Id_Doc { get; set; }
+    [Required]
+    [Column("ID_DOC")]
+    public int Id_Doc { get; set; }
 
-            [Column("ID_ROL")]
-            public int Id_Rol { get; set; }
+    [Required]
+    [Column("ID_ROL")]
+    public int Id_Rol { get; set; }
 
-            [Column("NOMBRE_USER")]
-            public string Nombre_User { get; set; }
+    [Required]
+    [StringLength(100)]
+    [Column("NOMBRE_USER")]
+    public string Nombre_User { get; set; }
 
-            [Column("EDAD_USER")]
-            public int Edad_User { get; set; }
+    [Required]
+    [Range(1, 120)]
+    [Column("EDAD_USER")]
+    public int Edad_User { get; set; }
 
-            [Column("NUMDOC_USER")]
-            public int NumDoc_User { get; set; }
+    [Required]
+    [Column("NUMDOC_USER")]
+    public int NumDoc_User { get; set; }
 
-            [Column("DATE_USER")]
-            public DateTime Date_User { get; set; }
+    [Required]
+    [Column("DATE_USER")]
+    public DateTime Date_User { get; set; }
 
-            [Column("EMAIL_USER")]
-            public string Email_User { get; set; }
+    [Required]
+    [EmailAddress]
+    [Column("EMAIL_USER")]
+    public string Email_User { get; set; }
 
-            [Column("NUMTEL_USER")]
-            public string NumTel_User { get; set; }
+    [Required]
+    [Phone]
+    [Column("NUMTEL_USER")]
+    public string NumTel_User { get; set; }
 
-            public Documentos Documentos { get; set; }
-            public Roles Roles { get; set; }
-    }
+    [ForeignKey("Id_Doc")]
+    public Documentos Documentos { get; set; }
+    [ForeignKey("Id_Rol")]
+    public Roles Roles { get; set; }
 }
